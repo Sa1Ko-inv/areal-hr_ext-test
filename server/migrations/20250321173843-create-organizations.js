07ea60d8ba69e12dbf8a88893012e552da594e3a
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-03-24 17:26:30 +0300

    chore(.env.exaple and naming table on BD): removed .env.example from .gitignore and changed the name of tables and columns in the DB using snake_case

--- server/migrations/20250321173843-create-organizations.js
+++ server/migrations/20250321173843-create-organizations.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Organizations', {
+    await queryInterface.createTable('organizations', {
         id: {
             type: Sequelize.INTEGER,
             primaryKey: true,
