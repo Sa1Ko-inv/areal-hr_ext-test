58074c60b0d35174403ca68870f6c6dc0755a8cf
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-04-19 11:14:43 +0300

    fix: Fixed close userDeleteHistory

--- client/src/components/user/userModal/userDeleteHistory.vue
+++ client/src/components/user/userModal/userDeleteHistory.vue
@@ -53,7 +53,7 @@ const totalPages = computed(() => {
 <template>
   <div class="delete-history-modal">
     <h4>История удаления</h4>
-    <MyButton class="close-btn" @click="cancel" title="Закрыть">×</MyButton>
+    <MyButton class="close-btn" @click="props.cancel" title="Закрыть">×</MyButton>
 
     <div v-if="deleteHistory.length === 0" class="no-data">
       Нет записей об удаленных должностях.
